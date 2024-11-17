d55e77d31ecd75cbdb2676023c4df5d5c9f1c517
aciumalina <aciumalina@yahoo.com>
2024-11-17 16:35:01 +0200

    added questions store as singleton

--- src/main/java/org/example/store/QuestionsStore.java
+++ src/main/java/org/example/store/QuestionsStore.java
@@ -5,9 +5,23 @@ import org.example.models.Question;
 import java.util.HashMap;
 
 public class QuestionsStore {
-    public HashMap<Integer, Question> questions = new HashMap<>();
+    public HashMap<Integer, Question> questions;
 
     public static int id = 0;
+
+    private static QuestionsStore single_instance = null;
+
+    private QuestionsStore(HashMap<Integer, Question> questions) {
+        this.questions = questions;
+    }
+
+    public static synchronized QuestionsStore getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new QuestionsStore(new HashMap<>());
+
+        return single_instance;
+    }
 
     public Question addQuestion(Question question){
         questions.put(++id,  question);
